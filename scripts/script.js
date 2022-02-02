7d3503870e1e74cf9039b9e760dbcdeb2fd30dde
lsanchez8719 <lsanchez8719@gmail.com>
2022-02-02 23:06:25 +0200

    fixes added aria labels and deleted 404

--- scripts/script.js
+++ scripts/script.js
@@ -1,15 +1,15 @@
 
-const openButton = document.querySelector('.button__add');
+const openButton = document.querySelector('.button__edit');
 const popup = document.querySelector('.popup');
 const closeButton = popup.querySelector('.button__close');
 
 function toggleForm() {
-  popup.classList.toggle('.popup_open');
+  popup.classList.toggle('.popup');
 }
 
 openFormButton.addEventListener('click', toggleForm);
 closeButton.addEventListener('click', toggleForm);
 
-editButton.addEventListener("click", handleEditButtonClick);
-popup.addEventListener("submit", handleEditFormSubmit);
-closeButton.addEventListener("click", () => closePopup());
+//editButton.addEventListener("click", handleEditButtonClick);
+//popup.addEventListener("submit", handleEditFormSubmit);
+//closeButton.addEventListener("click", () => closePopup());
