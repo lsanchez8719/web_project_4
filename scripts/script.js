637c2f7944faceb01f91b8d69b3b89a67139f187
lsanchez8719 <lsanchez8719@gmail.com>
2022-02-02 21:42:54 +0200

    debuging  fixes

--- scripts/script.js
+++ scripts/script.js
@@ -1,10 +1,10 @@
 
-const openFormButton = document.querySelector('.button_type_close');
+const openFormButton = document.querySelector('.button__add');
 const popup = document.querySelector('.popup');
-const closeButton = popup.querySelector('');
+const closeButton = popup.querySelector('.button__close');
 
 function toggleForm() {
-  popup.classList.toggle('popup_visible');
+  popup.classList.toggle('.popup_open');
 }
 
 openFormButton.addEventListener('click', toggleForm);
