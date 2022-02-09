2a8d6a8c322b788279cb1c94f5c7587fcd107a29
lsanchez8719 <lsanchez8719@gmail.com>
2022-02-09 20:50:17 +0200

    fixed project for review

--- scripts/script.js
+++ scripts/script.js
@@ -5,20 +5,20 @@ const profileProfession = profileInfo.querySelector('.profile__role');
 
 const profileForm = document.forms.profileForm;
 const profilePopup = document.querySelector('.popup');
-const editButton = document.querySelector ('.profile__button-edit');
+const editButton = document.querySelector('.profile__button-edit');
 const saveButton = profilePopup.querySelector('.popup__button-submit');
 const closeButton = profilePopup.querySelector('.popup__button-close');
 let inputName = profilePopup.querySelector('.popup__input_type_name');
 let inputProfession = profilePopup.querySelector('.popup__input_type_role');
 
 //functions
-function formLoadUp () {
+function formLoadUp() {
   profilePopup.classList.toggle('popup_opened');
   inputName.value = profileName.textContent;
   inputProfession.value = profileProfession.textContent;
 }
 
-function closePopup () {
+function closePopup() {
   profilePopup.classList.remove('popup_opened');
 }
 
