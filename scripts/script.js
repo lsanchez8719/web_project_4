0575154dceb5a8554f085698d30a1ec96d158d6d
lsanchez8719 <lsanchez8719@gmail.com>
2022-02-18 20:05:49 +0200

    Feat/ initial cardsjavascript

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,35 @@
+/* -------------------------------------------------------------------------- */
+/*                              Six initial cards                             */
+/* -------------------------------------------------------------------------- */
+
+const initialCards = [
+  {
+    name: "Yosemite Valley",
+    link: "https://code.s3.yandex.net/web-code/yosemite.jpg",
+  },
+  {
+    name: "Lake Louise",
+    link: "https://code.s3.yandex.net/web-code/lake-louise.jpg",
+  },
+  {
+    name: "Bald Mountains",
+    link: "https://code.s3.yandex.net/web-code/bald-mountains.jpg",
+  },
+  {
+    name: "Latemar",
+    link: "https://code.s3.yandex.net/web-code/latemar.jpg",
+  },
+  {
+    name: "Vanoise National Park",
+    link: "https://code.s3.yandex.net/web-code/vanoise.jpg",
+  },
+  {
+    name: "Lago di Braies",
+    link: "https://code.s3.yandex.net/web-code/lago.jpg",
+  },
+];
+
+
 const profile = document.querySelector(".profile"); //+
 const profileInfo = document.querySelector('.profile__info');
 const profileName = profileInfo.querySelector('.profile__name-text');
