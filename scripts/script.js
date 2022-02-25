4e9766d1b573c1c8e2b85abe12be3a3e875d877e
lsanchez8719 <lsanchez8719@gmail.com>
2022-02-25 19:39:40 +0200

    fixed review Bem comment

--- scripts/script.js
+++ scripts/script.js
@@ -19,7 +19,7 @@ const buttonCloseAddCard = document.querySelector('.button_location_add-card');
 const buttonClosePreview = document.querySelector('.button_location_preview');
 
 
-const formProfile = document.querySelector('.popup_form_type-profile');
+const formProfile = document.querySelector('.popup_form_profile');
 const formAddCard = document.querySelector('.popup__form_type_add-card');
 
 const imagePreview = document.querySelector('.popup__preview-image');
